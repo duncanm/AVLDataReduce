877db8581fec83eca4624214296b6d7678febbf1
duncanm <github@duncanmargetts.com>
2018-02-24 20:52:22 +1030

    Update processavl.js

--- processavl.js
+++ processavl.js
@@ -26,15 +26,15 @@ var LineByLineReader = require('line-by-line'),
 
 
 function getDistance(lat1, lon1, lat2, lon2) {
-  var R = 6371; // Radius of the earth in kilometers
-  var dLat = deg2rad(lat2 - lat1); // deg2rad below
+  var R = 6371; 
+  var dLat = deg2rad(lat2 - lat1);
   var dLon = deg2rad(lon2 - lon1);
   var a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
     Math.sin(dLon / 2) * Math.sin(dLon / 2);
   var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  var d = R * c; // Distance in KM
+  var d = R * c; 
   return d;
 }
 
